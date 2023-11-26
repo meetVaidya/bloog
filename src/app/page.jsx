181664a7c5e939edcab2650c690ff8779154178d
meext <vaidyameet2004@gmail.com>
2023-11-26 15:49:57 +0530

    final commit before deploying

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
             <Featured />
             <CategoryList />
             <div className={styles.content}>
-                <CardList page={page} />
+                <CardList />
                 <Menu />
             </div>
         </div>
