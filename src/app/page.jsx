eaf222a7e942b1814ce59d28a92ee70ae1b40a5f
meext <vaidyameet2004@gmail.com>
2023-12-04 08:28:46 +0530

    Revert "Refactor code to include ThemeContext and"
    This reverts commit e63f5d9e5da0b64ae6d7365799c7716614e760a1.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,10 @@
-import { ThemeContext, ThemeContextProvider } from '@/context/ThemeContext';
 import styles from './homepage.module.css';
 import LandingPage from './landingPage/LandingPage';
 
 export default function Home() {
     return (
-        <ThemeContextProvider>
-            <ThemeContext>
-                <div className={styles.container}>
-                    <LandingPage />
-                </div>
-            </ThemeContext>
-        </ThemeContextProvider>
+        <div className={styles.container}>
+            <LandingPage />
+        </div>
     );
 }
